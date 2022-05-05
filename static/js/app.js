4f41d2e673652e9cbd66058a38c07573c8f3ee67
jeffmodarres <99626528+jeffmodarres@users.noreply.github.com>
2022-05-04 21:25:17 -0400

    end of 11.5.2

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+// Lets learn JavaScript
+
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody")
+
+function buildTable(data) {
+    tbody.html("")
+
+    data.forEach(datarow => {
+        let row = tbody.append("tr") 
+        
+        Object.values(datarow).forEach( val=> {
+            let cell = row.append("td");
+            cell.text(val);
+         })
+    });
+}
+
